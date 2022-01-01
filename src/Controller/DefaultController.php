6cb73f6268c48263d8755614a4fff418ff7aaa2a
Vasiliy Pivovarov <vasiliy.mv.obzor@gmail.com>
2022-01-02 01:19:31 +0300

    Created login and registration pages Created profile page Created profile edit page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,21 +5,17 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Form\EditProductFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
+    #[Route('/', name: 'main_homepage')]
     public function index(): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
         $productList = $entityManager->getRepository(Product::class)->findAll();
-        dd($productList);
 
         return $this->render('main/default/index.html.twig', []);
     }
